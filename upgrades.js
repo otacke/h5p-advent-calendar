a771f25fc319ad594a0ccbd871515a35b6b5a84d
Oliver Tacke <o.tacke@posteo.de>
2020-11-25 16:41:49 +0100

    Fix upgrade

--- upgrades.js
+++ upgrades.js
@@ -8,7 +8,7 @@ H5PUpgrades['H5P.AdventCalendar'] = (function () {
         parameters.a11y = parameters.a11y || {};
 
         parameters.a11y.mute = (parameters.l10n && parameters.l10n.mute) ? parameters.l10n.mute : 'Mute audio';
-        parameters.a11y.unmute = (parameters.l10n && parameters.l10n.unmute) ? parameters.l10n.mute : 'Unmute audio';
+        parameters.a11y.unmute = (parameters.l10n && parameters.l10n.unmute) ? parameters.l10n.unmute : 'Unmute audio';
         parameters.a11y.closeWindow = (parameters.l10n && parameters.l10n.closeWindow) ? parameters.l10n.closeWindow : 'Close window';
 
         delete parameters.l10n;
